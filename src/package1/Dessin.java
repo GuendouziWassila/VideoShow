771a3993c28c2757f99b2deba7637f9cbbc9f152
Guendouzi <guendouziwassila@gmail.com>
2021-12-22 12:28:23 +0100

    initial

---
+++ src/package1/Dessin.java
@@ -0,0 +1,90 @@
+package package1;
+
+import java.awt.Graphics;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import javax.swing.JPanel;
+
+
+public class Dessin extends JPanel{
+	/**
+     * stocke la liste des trains ayant �t� ajout�es à cette zone de dessin.
+     */
+    private final List<TrainCercle> listeTrain = new CopyOnWriteArrayList();
+
+    /**
+     * retourne la largeur de la zone de dessin.
+     *
+     * @return la largeur.
+     */
+    public int getLargeur() {
+        return getWidth();
+    }
+
+    /**
+     * retourne la hauteur de la zone de dessin.
+     *
+     * @return la hauteur.
+     */
+    public int getHauteur() {
+        return getHeight();
+    }
+
+    /**
+     * ajoute un train cercle à la zone de dessin.
+     *
+     * @param ch le train  à ajouter au Dessin
+     * @see train
+     */
+    public void ajouterObjet(TrainCercle t) {
+
+        if (!listeTrain.contains(t)) {
+            // l'objet n'est pas déjà dans la liste
+            // on le rajoute a la liste des objets du dessin
+        	listeTrain.add(t);
+            // le dessin se réaffiche
+            repaint();
+            this.pause(10);
+        }
+    }
+
+    /**
+     * temporisation de l'animation.
+     *
+     * @param duree delai de temporisation en ms.
+     */
+    public void pause(int duree) {
+        try {
+            Thread.sleep(duree);
+        } catch (InterruptedException e) {
+        }
+    }
+
+    /**
+     * affiche la zone de dessin et son contenu
+     *
+     * @param g le contexte graphique
+     */
+  
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        //  dessiner les trains que contient le dessin
+        for (TrainCercle c : listeTrain) {
+            c.dessiner(g);
+        }
+    }
+
+    /**
+     * fait deplacer d'un déplacement élémentaire chacun des train cercles
+     * que la zone de dessin contient.
+     */
+    public void animer() {
+        //  dessiner les Objets que contient le dessin
+        for (TrainCercle c : listeTrain) {
+        	c.deplacer();
+            
+        }
+    }
+
+}
---
+++ src/package1/LancerApp1.java
@@ -0,0 +1,52 @@
+package package1;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+public class LancerApp1 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedImage imgVador= ImageIO.read(new File("images/person2.png"));
+		BufferedImage imgLeila= ImageIO.read(new File("images/person1.png"));
+		
+		// création de la fenêtre de l'application
+		JFrame laFenetre= new JFrame("Animation Train, etc.");
+		laFenetre.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		laFenetre.setSize(512, 512);
+		
+		// création de la zône de dessin dans la fenêtre
+		Dessin d = new Dessin();
+		laFenetre.getContentPane().add(d);
+		
+		// affiche la fenêtre
+		laFenetre.setVisible(true);
+		
+		// les trains de cercles avec image et en couleur
+		TrainCercle[] lesTrains= new TrainCercle[10];
+		lesTrains[0] = new TrainCercleImage(d, 10, imgVador); d.ajouterObjet(lesTrains[0]);
+		lesTrains[1] = new TrainCercleImage(d, 10, imgLeila); d.ajouterObjet(lesTrains[1]);
+		for (int i = 2; i < 10; i++) {
+			lesTrains[i] = new TrainCercleCouleur(new Color((float) Math.random(), (float) Math.random(),
+		(float) Math.random()), d, 10, 10);
+		d.ajouterObjet(lesTrains[i]);
+		}
+		
+		
+		while(true) {
+		// la zone de dessin se réaffiche
+		d.repaint();
+		// un temps de pause pour avoir le temps de voir le nouveau dessin
+		d.pause(50);
+		//réaliser à tous les trains un déplacement élémentaire
+		d.animer();
+		
+		}
+
+	}
+
+}
